0ae0ff916937233cc81108cc3b16bea9eb40b5da
Carol Gardiner <carolan.gardiner@gmail.com>
2019-03-14 11:11:49 +0000

    add feature tests for normal items

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -12,6 +12,7 @@ class GildedRose
             item.quality = item.quality - 1
           end
         end
+
       else
         if item.quality < 50
           item.quality = item.quality + 1
@@ -29,9 +30,11 @@ class GildedRose
           end
         end
       end
+
       if item.name != "Sulfuras, Hand of Ragnaros"
         item.sell_in = item.sell_in - 1
       end
+
       if item.sell_in < 0
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
@@ -49,6 +52,7 @@ class GildedRose
           end
         end
       end
+      
     end
   end
 end
--- spec/features/rose_feature_spec.rb
+++ spec/features/rose_feature_spec.rb
@@ -3,11 +3,40 @@ require 'gilded_rose'
 describe GildedRose do
 
   describe "#update_quality" do
-    it "does not change the name" do
-      items = [Item.new("foo", 0, 0)]
-      GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
-    end
-  end
+   
+      context "normal items" do 
+        before(:each) do 
+          @items = [Item.new("foo", 10, 20)]
+          @gildedrose = GildedRose.new(@items)
+        end 
+  
+        it "does not change the name" do
+          @gildedrose.update_quality()
+            expect(@items[0].name).to eq "foo"
+        end
+  
+        it "the sellin should reduce by 1 each day" do
+          @gildedrose.update_quality()
+            expect(@items[0].sell_in).to eq 9
+        end 
 
+        it "checks that items update correctly multiple times" do 
+          @gildedrose.update_quality()
+          @gildedrose.update_quality()
+            expect(@items[0].sell_in).to eq 8 
+        end
+  
+        it "quality should reduce by 2 after sellin date" do 
+          11.times { @gildedrose.update_quality() }
+            expect(@items[0].quality).to eq 8
+        end 
+    
+        it "value should never fall below 0 even after sellin" do 
+          14.times { @gildedrose.update_quality() }
+            expect(@items[0].quality).to be >= 0
+        end 
+        
+
+      end
+    end
 end
